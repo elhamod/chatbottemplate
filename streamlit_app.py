fb2e4137f42099cf17fc62671a10bc8efb56e16d
elhamod <55500282+elhamod@users.noreply.github.com>
2024-10-01 18:55:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-chain = prompt | ChatOpenAI(st.secrets["OPENAI_API_KEY"])
+chain = prompt | ChatOpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 chain_with_history = RunnableWithMessageHistory(
     chain,
